cddb4ca37daa093199db586b1ab792b173ff010f
BunnY <w@bunnywong.com>
2020-01-17 15:16:48 +0800

    hardcode footer for i18n :)

--- wp-content/themes/naslaan/footer.php
+++ wp-content/themes/naslaan/footer.php
@@ -288,20 +288,44 @@ if( naslaan_helpers::naslaan_unyson_check() ) {
 						</div>
 					</div>
 				</div>
-				<?php } ?> 
-				
+				<?php } ?>
+
+        <?php // just custom hardcode:) ?>
+        <div class="bottom-section">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="text-container">
+                  <?php // CUSTOM script ?>
+                  <?php if(ICL_LANGUAGE_CODE === 'en'): ?>
+                    <p>Copyright © 2019 tickcopy.com</p>
+                    <p class="p1"><span class="s1"><b>Risk Warning</b>: Trading Forex and Leveraged Financial Instruments involves significant risk and can result in the loss of your invested capital. You should not invest more than you can afford to lose and should ensure that you fully understand the risks involved. Trading leveraged products may not be suitable for all investors. Before trading, please take into consideration your level of experience, investment objectives and seek independent financial advice if necessary. It is the responsibility of the Client to ascertain whether he/she is permitted to use the services of the Tickcopy Limited based on the legal requirements in his/her country of residence.</span></p>
+                    <p class="p3"><span class="s1"><b>Regional restrictions:</b> Tickcopy Limited does not accept applications from residents of the U.S, Canada, Sudan, Syria, North Korea, United Kingdom, Israel and Islamic Republic of Iran. The information on this site is not directed at residents in any country or jurisdiction where such distribution or use would be contrary to local law or regulation.</span></p>
+                    <p class="p3"><span class="s1">Tickcopy.com is owned by and operated by Tickcopy Limited. Clients must be at least 18 years old to use the services Tickcopy Limited.</span></p>
+                  <?php else: ?>
+                    <p>Copyright © 2019 tickcopy.com</p>
+                    <p class="p1"><span class="s1"><b>风险警告</b>: 交易外汇和杠杆金融工具涉及重大风险，并可能导致您投资资本的损失。 您不应投资超过承受的损失，并应确保您完全了解所涉及的风险。 交易杠杆产品可能并不适合所有投资者。 在交易之前，请考虑您的经验水平，投资目标并在必要时寻求独立的财务建议。 客户有责任根据其居住国家/地区的法律要求，确定是否允许其使用 Tickcopy Limited 的服务。</span></p>
+                    <p class="p3"><span class="s1"><b>区域限制:</b> Tickcopy Limited 不接受美国，加拿大，苏丹，叙利亚，朝鲜，英国，以色列和伊朗伊斯兰共和国居民的申请。 本网站上的信息并不针对任何分布或使用违反当地法律或法规的国家或地区的居民。</span></p>
+                    <p class="p3"><span class="s1">Tickcopy.com 由 Tickcopy Limited拥有并运营。 客户必须年满18岁才能使用 Tickcopy Limited 服务。</span></p>
+                  <?php endif; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
 			</div>
-			<?php } ?> 
-		
+			<?php } ?>
+
 	</div>
-	
+
 	<?php
-	
+
 		echo "<div class='sticky-slide-panel-toggle'>";
 		naslaan_front_functions::naslaan_slide_panel_toggle();
-		echo "</div>";	
+		echo "</div>";
 	?>
-	
+
 	<?php if($back_to_top_visibility=='enabled'){ ?>
 	<a href="#" id="back-to-top" title="<?php esc_attr_e('Back to top', 'naslaan'); ?>"><div class="icon icon-arrow-up-7"></div></a>
 	<?php } ?>
